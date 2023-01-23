3fda4615115a3b5bd22677deeae6d6cdfeea238f
Raphael Fleischlin <git@raeffs.dev>
2023-01-23 19:20:04 +0100

    add powershell config

---
+++ powershell.profile.ps1
@@ -0,0 +1,27 @@
+
+Function .. { Set-Location .. }
+
+Function Open-Explorer {
+  Param(
+    [string]$path='.'
+  )
+
+  explorer $path
+}
+
+Function Open-VsCode {
+  Param(
+    [string]$path='.'
+  )
+
+  code $path
+}
+
+Set-Alias -Name e -Value Open-Explorer
+Set-Alias -Name c -Value Open-VsCode
+Set-Alias -Name g -Value git
+
+Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
+
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/raeffs/dotfiles-windows-11/main/oh-my-posh.config.json' | Invoke-Expression
