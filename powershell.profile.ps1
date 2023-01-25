2f42b4e5a332d0c1cdb1b6e26067c9381a37413c
Raphael Fleischlin <git@raeffs.dev>
2023-01-25 17:39:31 +0100

    add table header format

--- powershell.profile.ps1
+++ powershell.profile.ps1
@@ -25,3 +25,5 @@ Import-Module -Name Terminal-Icons
 Import-Module -Name posh-git
 
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/raeffs/dotfiles-windows-11/main/oh-my-posh.config.json' | Invoke-Expression
+
+$PSStyle.Formatting.TableHeader = "`e[3;37m"
